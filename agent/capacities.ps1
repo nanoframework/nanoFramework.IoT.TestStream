7eb32be78f2dc4dc8fd9cceda58890d3ece570dd
Laurent Ellerbach <laurent@ellerbach.net>
2024-10-17 11:02:10 +0200

    adding capacity setup and native docs

---
+++ agent/capacities.ps1
@@ -0,0 +1,39 @@
+# Check if AZP_TOKEN_FILE is not set
+if (-not $env:AZP_TOKEN_FILE) {
+    # Check if AZP_TOKEN is not set
+    if (-not $env:AZP_TOKEN) {
+        Write-Error "error: missing AZP_TOKEN environment variable"
+        exit 1
+    }
+
+    # Set AZP_TOKEN_FILE and write AZP_TOKEN to it
+    $env:AZP_TOKEN_FILE = ".token"
+    Set-Content -Path $env:AZP_TOKEN_FILE -Value $env:AZP_TOKEN -NoNewline
+}
+
+# Unset AZP_TOKEN
+Remove-Item -Path Env:AZP_TOKEN
+
+# Read the .agent file and extract the serverUrl and poolName properties
+$agentConfig = Get-Content -Raw -Path ".agent" | ConvertFrom-Json
+$serverUrl = $agentConfig.serverUrl
+$poolName = $agentConfig.poolName
+
+# setting up the capabilities
+$AZP_POOL_AGENTS = Invoke-RestMethod -Uri "$serverUrl/_apis/distributedtask/pools?poolName=$poolName&api-version=7.2-preview.1" -Headers @{Authorization=("Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("user:" + (Get-Content -Path $env:AZP_TOKEN_FILE))))} -Method Get
+$AZP_POOL_ID = $AZP_POOL_AGENTS.value[0].id
+
+# URL encode the AZP_AGENT_NAME environment variable
+$encoded_name = [System.Web.HttpUtility]::UrlEncode($env:AZP_AGENT_NAME)
+
+# Print the encoded name
+$AZP_POOL_AGENTS = Invoke-RestMethod -Uri "$serverUrl/_apis/distributedtask/pools/$AZP_POOL_ID/agents?agentName=$encoded_name&api-version=7.2-preview.1" -Headers @{Authorization=("Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("user:" + (Get-Content -Path $env:AZP_TOKEN_FILE))))} -Method Get
+$AZP_AGENT_ID = $AZP_POOL_AGENTS.value[0].id
+
+# Sending the custom capabilities
+$capabilities = Get-Content -Raw -Path "configuration.json" | ConvertFrom-Json | Select-Object -ExpandProperty capabilities | ConvertTo-Json -Compress
+$AZP_POOL_AGENTS = Invoke-RestMethod -Uri "$serverUrl/_apis/distributedtask/pools/$AZP_POOL_ID/agents/$AZP_AGENT_ID/usercapabilities?api-version=7.2-preview.1" -Headers @{Authorization=("Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("user:" + (Get-Content -Path $env:AZP_TOKEN_FILE))))} -Method Put -ContentType "application/json" -Body $capabilities
+Write-Output "Capabilities set: $capabilities"
+
+# Remove the token file
+Remove-Item -Path $env:AZP_TOKEN_FILE
--- native-agent.md
+++ native-agent.md
@@ -12,3 +12,49 @@ You will be prompted for:
 * the agent poolo is: `TestStream`
 * use the default `_work` directory
 * select if you want or not install the agent as a service or not
+
+Copy the [capacities.ps1](./agent/capacities.ps1) file to the C`:\agent` folder.
+
+## Pre requirements
+
+You'll need to install on the machine:
+
+* [.NET 8.0](https://dotnet.microsoft.com/en-us/download)
+* [.NET Framework 4.8](https://dotnet.microsoft.com/en-us/download/dotnet-framework/net48)
+
+## Setup capacities
+
+Once you've been running the previous configuration, you'll need to create a `configuration.json` file in the `C:\agent` directory.
+
+The configuration should like the devices you have and the serial ports associated. As an example:
+
+```json
+{
+  "capabilities": {
+    "ESP32_REV0": "COM7"
+  }
+}
+```
+
+You can add many boards and firmware as you want. It's important to set properly the serial port for each of them. With the same hardware, you can have multiple firmware.
+In that case, they'll share the same serial port and that's perfectly ok. At a single point of time, only 1 remote pipeline is running, so there won't be any clonflict with the serial ports used.
+
+Then in powershell, run the following commands:
+
+```powershell
+cd C:\agent
+$env:AZP_TOKEN="yourlongtoken"
+.\capacities.ps1
+```
+
+The token will only be used during the setup of the capacity and will be deleted after from the environement and from a temporary file that will be created.
+
+> [!Important]
+> You will have to run this script every time you change the capabilities, meaning, adding or removing hardware.
+
+## Run the agent
+
+Depending on what you have chosen during the installation, it will either run as a service, either as on demand. You can as well create a task.
+
+> [!Important]
+> When the PAT token expires, you'll need to rerun the configuration.
